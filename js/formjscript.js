51c95a79c10c05985df142d9ca0d5e441fe6ea15
sagar-wal <sagaragarwal612@gmail.com>
2020-04-03 23:03:44 +0530

    changes

--- js/formjscript.js
+++ js/formjscript.js
@@ -11,7 +11,7 @@ createform.appendChild(line);
 var linebreak = document.createElement('br');
 createform.appendChild(linebreak);
 var namelabel = document.createElement('label'); // Create Label for Name Field
-namelabel.innerHTML = "Your Name : "; // Set Field Labels
+namelabel.innerHTML = "Name : "; // Set Field Labels
 createform.appendChild(namelabel);
 var inputelement = document.createElement('input'); // Create Input Field for Name
 inputelement.setAttribute("type", "text");
@@ -19,30 +19,30 @@ inputelement.setAttribute("name", "dname");
 createform.appendChild(inputelement);
 var linebreak = document.createElement('br');
 createform.appendChild(linebreak);
-var usernamelabel = document.createElement('label'); // Create Label for Address Field
-usernamelabel.innerHTML = "User Name : "; // Set Field Labels
-createform.appendChild(usernamelabel);
+var emaillabel = document.createElement('label'); // Create Label for Address Field
+emaillabel.innerHTML = "Email Address : "; // Set Field Labels
+createform.appendChild(emaillabel);
 var inputelement = document.createElement('input'); // Create Input Field for Name
 inputelement.setAttribute("type", "text");
-inputelement.setAttribute("name", "usrname");
+inputelement.setAttribute("name", "email");
 createform.appendChild(inputelement);
 var linebreak = document.createElement('br');
 createform.appendChild(linebreak);
-var pswdlabel = document.createElement('label'); // Create Label for Password Field
-pswdlabel.innerHTML = "Password : "; // Set Field Labels
-createform.appendChild(pswdlabel);
+var branchlabel = document.createElement('label'); // Create Label for Password Field
+branchlabel.innerHTML = "Branch : "; // Set Field Labels
+createform.appendChild(branchlabel);
 var inputelement = document.createElement('input'); // Create Input Field for Password
-inputelement.setAttribute("type", "password");
-inputelement.setAttribute("name", "psswd");
+inputelement.setAttribute("type", "text");
+inputelement.setAttribute("name", "branch");
 createform.appendChild(inputelement);
 var linebreak = document.createElement('br');
 createform.appendChild(linebreak);
-var doblabel = document.createElement('label'); // Create Label for date of birth
-doblabel.innerHTML = "Your Date of Birth : "; // Set Field Labels
-createform.appendChild(doblabel);
+var contactlabel = document.createElement('label'); // Create Label for date of birth
+contactlabel.innerHTML = "Contact Number : "; // Set Field Labels
+createform.appendChild(contactlabel);
 var inputelement = document.createElement('input'); // Create Input Field for date of birth
 inputelement.setAttribute("type", "text");
-inputelement.setAttribute("name", "dob");
+inputelement.setAttribute("name", "contact");
 createform.appendChild(inputelement);
 //code for date picker:
 $(inputelement).datepicker({minDate: new Date(),
